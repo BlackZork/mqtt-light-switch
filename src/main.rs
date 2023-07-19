7b9928e9a3773ee59260e9139d3f238baf07ca74
Lukasz Michalski <lm@zork.pl>
2023-07-19 12:39:11 +0200

    mqtt hellow world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rumqttc::{MqttOptions, Client, QoS};
+
 fn main() {
-    println!("Hello, world!");
+    let mqttoptions = MqttOptions::new("mqtt-light-switches", "zork.pl", 1883);
+
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    
+    client.subscribe("bedroom/switch/ceiling_door/state", QoS::AtLeastOnce).unwrap();
+
+    for (_i, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
+    }    
 }
